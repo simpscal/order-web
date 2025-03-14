f60c9ea73fed51ea52af5dbcc6817c3e07c95881
Tu Phan <pt27122000@gmail.com>
2025-03-15 00:40:20 +0700

    feat: add drawer component to header and enhance styling

--- src/app/layouts/default/components/header/header.component.ts
+++ src/app/layouts/default/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
 import { ToolbarModule } from 'primeng/toolbar';
 
 import { PAGE_ROUTES } from '@app/shared/constants';
@@ -8,13 +9,15 @@ import { PAGE_ROUTES } from '@app/shared/constants';
 @Component({
     selector: 'app-default-layout-header',
     standalone: true,
-    imports: [ToolbarModule, ButtonModule, RouterLink, RouterLinkActive],
+    imports: [ToolbarModule, ButtonModule, DrawerModule, RouterLink, RouterLinkActive],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DefaultLayoutHeaderComponent {
     PAGE_ROUTES = PAGE_ROUTES;
+
+    drawerVisible = signal(false);
 
     onLogin() {}
 }
